8d277117dea8169d61d939a2bea4781c1c08a1db
Nwosa <obere4u@gmail.com>
2023-06-02 18:01:43 +0100

    Added the clearInput and addItemToShoppingList

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-//using firebase
+//setting up firebase firebase
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://cart-companion-c7725-default-rtdb.firebaseio.com/",
 };
 
 const app = initializeApp(appSettings);
-const database = getDatabase(app)
-const shoppingListDB = ref(database, "shoppingList")
+const database = getDatabase(app);
+const shoppingListDB = ref(database, "shoppingList"); //create the reference in database
 
-const inputFieldEl = document.getElementById("input-field");
+//selectors
 const addButtonEl = document.getElementById("add-button");
+let inputFieldEl = document.getElementById("input-field");
+let shoppingListEl = document.getElementById("shopping-list");
+let item;
 
 addButtonEl.addEventListener("click", function () {
-  let inputValue = inputFieldEl.value;
+  item = inputFieldEl.value;
 
-  push(shoppingListDB, inputValue)
+  push(shoppingListDB, item); //push to database
 
-  console.log(inputValue);
+  clearInputField();
+  addItemToShoppingList();
+
+  console.log(item);
 });
+
+function clearInputField() {
+  inputFieldEl.value = "";
+}
+
+function addItemToShoppingList() {
+  shoppingListEl.innerHTML += `<li>${item}</li>`;
+}
