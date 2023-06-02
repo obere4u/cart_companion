5b1b26e9be53d23f86799d5884a93b496530499f
Nwosa <obere4u@gmail.com>
2023-06-02 14:59:48 +0100

    Making th JS file functional and created database

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-// Challenge: Import 'initializeApp' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-// Challenge: Import 'getDatabase' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+//using firebase
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseUrl: "https://cart-companion-c7725-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app)
+const shoppingListDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+
+  push(shoppingListDB, inputValue)
 
   console.log(inputValue);
 });
